9ff6d38fbb67d229b37744a86f5e3680882ea597
RobertoMaximo <rquisped@fi.uba.ar>
2026-03-25 17:00:14 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-FILENAME="FILENAME"
-export FILENAME 
-echo "seleccione una opción:"
+echo "Menu"
 echo "1) Crear entorno"
 echo "2) Correr proceso"
 echo "3) Ordenar por numero de padron"
-echo "4) Ordenar por notas"
+echo "4) Ordenar por notas mas altas"
 echo "5) Buscar padron"
 echo "6) Salir"
 
-read opcion
+read -p "Elija una opcion:" opcion
 
-if [ "$opcion" == 1 ]; then
- bash ~/crear_entorno.sh
-elif [ "$opcion" == 2 ]; then
- bash ~/consolidar.sh &
- echo "el proceso se esta realizando"
-elif [ "$opcion" == 3 ]; then
- bash ~/orden_padron.sh 
-elif [ "$opcion" == 4 ]; then
- bash ~/ordenar_notas.sh
-elif [ "$opcion" == 5 ]; then
- bash ~/buscar_padron.sh 
-elif [ "$opcion" == 6 ]; then 
- echo "Cerrando programa"
-fi
+case $opcion in
+    1)if [ -e "EPNro1" ]; then
+      echo "Ya creaste un entorno"
+      else
+      mkdir -p ~/EPNro1/entrada ~/EPNro1/salida ~/EPNro1/procesado
+      echo "Entorno creado"
+      fi
+      ;;
+
+      2)cp "$HOME/consolidar.sh" "$HOME/EPNro1/"
+      bash ~/EPNro1/consolidar.sh &
+      echo "Ejecutando proceso en segundo plano"
+      ;;
+
+      3) if [ -f ~/EPNro1/salida/FILENAME.txt ]; then
+      sort -nr -k 2 ~/EPNro1/salida/FILENAME.txt
+      else
+         echo "no existe el archivo"
+       fi
+       ;;
+
+       4) if [ -f ~/EPNro1/salida/FILENAME.txt ]; then
+      sort -nr -k 5 ~/EPNro1/salida/FILENAME.txt
+      else
+         echo "no existe el archivo"
+       fi
+       ;;
+
+       5)read -p "Ingrese un numero de padron:" pa
+       grep "$pa" ~/EPNro1/salida/FILENAME.txt 
+       ;;
+
+       esac
